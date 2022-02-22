e857d952df5b7240c1d04d19cac7ebc081c5aafe
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-22 18:13:09 +0530

    Test: 12

--- script.js
+++ script.js
@@ -2,8 +2,15 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 11
+// Test: 12
 
-let x = 'JavaScript | Knowledge Tests';
-x.test = true;
-console.log(x.test); // undefined
+
+function myFunction() {
+    let a = 5
+    if (a === 5) {
+        let b = 10
+    }
+    return b
+}
+
+console.log(myFunction()); // b is not defined
