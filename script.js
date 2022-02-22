0449e5f41132dd79a717871f3677b28e2df71fd1
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-22 17:52:19 +0530

    Test: 11

--- script.js
+++ script.js
@@ -2,12 +2,8 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 10
+// Test: 11
 
-const items = [1, 15, 2, 25, 3];
-const myFunction = (value) => value + 5;
-
-for (let i = 0; i < items.length; i++) {
-    myFunction(items[i]);
-}
-console.log(items); // [ 1, 15, 2, 25, 3 ]
+let x = 'JavaScript | Knowledge Tests';
+x.test = true;
+console.log(x.test); // undefined
