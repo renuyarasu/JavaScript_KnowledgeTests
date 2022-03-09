94bb55a81dffa4ad229261f893aa148ce99c1e9b
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-09 21:55:37 +0530

    Test: 16 - object

--- script.js
+++ script.js
@@ -2,15 +2,14 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 16 [October 20, 2021]
+// Test: 17 [October 21, 2021]
 // Think about what will be printed to the console after the program is executed.
 
-function check_age(age) {
-    if (age < 18) {
-        const message = 'Boy!'
-    } else {
-        const message = 'Men!'
-    }
-    return message;
+const person = {
+    name: 'VedaGna',
+    age: 5
 }
-console.log(check_age[35]); // undefined
+let city = person.city;
+city = 'Hyderabad';
+
+console.log(person); //{ name: 'VedaGna', age: 5 }
