237e351c1ea2deeb5cfbcbe3a658b6906d4f2db4
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-09 22:08:52 +0530

    Test: 19

--- script.js
+++ script.js
@@ -2,16 +2,19 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 18 [October 24, 2021]
+// Test: 19 [October 25, 2021]
 // Think about what will be printed to the console after the program is executed.
 
-let a = [1, 2, 3];
-let b = [1, 2, 3];
-let result = (a == b);
-console.log(result); // false
+
+function getItems(carList, ...args, favCar) {
+    return [...carList, ...args, favCar]
+}
+
+let result = getItems(['Lada', 'Audi'], 'Tata', 'BMW');
+console.log(result); // SyntaxError: Rest parameter must be last formal parameter
 
 /* 
-Explanation:
-The arrays are  not  equal, although they are the same  in content. This is because JavaScript stores two references to two places in RAM. 
-A different place in memory means the  arrays are different . 
+Explanation: https://telegra.ph/Zadacha-JS-10-25-2
+...args is an array containing all remaining arguments and can only be passed last! In this example, other parameters were the second parameter, not the last . 
+This will result in a syntax error.
 */
