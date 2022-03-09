556ba1b0b91f8f05183a303f9e91a2e4b043d370
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-09 21:59:59 +0530

    Test: 17

--- script.js
+++ script.js
@@ -2,14 +2,8 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 17 [October 21, 2021]
+// Test: 17 [October 23, 2021]
 // Think about what will be printed to the console after the program is executed.
 
-const person = {
-    name: 'VedaGna',
-    age: 5
-}
-let city = person.city;
-city = 'Hyderabad';
-
-console.log(person.city); //undefined
+const num = [] + false - null + true;
+console.log(num); // NaN
