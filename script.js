967a7500c3f070a5c894dc471337a73b251c1955
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-09 22:04:52 +0530

    Test: 18 - Explanation

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ let a = [1, 2, 3];
 let b = [1, 2, 3];
 let result = (a == b);
 console.log(result); // false
+
+/* 
+Explanation:
+The arrays are  not  equal, although they are the same  in content. This is because JavaScript stores two references to two places in RAM. 
+A different place in memory means the  arrays are different . 
+*/
