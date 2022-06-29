15d681cd9afcd10bae160d144172f4ce6cff5f1e
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-29 21:55:26 +0530

    Test: 22

--- script.js
+++ script.js
@@ -2,15 +2,22 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 21 [October 27, 2021]
+// Test: 22 [October 28, 2021]
 
-const func = ({ a, b, c }) => {
-    console.log(a, b, c);
-};
-func(1, 2, 3) // undefined undefined undefined
+function nums(a, b) {
+    if (a > b) {
+        console.log('a > b');
+    }
+    else {
+        console.log('a < b');
+    }
+    return a + b;
+}
+console.log(nums(6, 3));
+console.log(nums(2, 4));
 
 /* 
 Explanation:
-In the parameters of the arrow function, we pass an object with fields a, b and c. And we are trying to display the variables a, b and c , 
-which are not declared, so we get undefined 3 times. */
+In JavaScript, we don't have to explicitly specify ; at the end of each line, however the JS engine still adds them after the statements. For example, statements can be variables or keywords such as throw, return, break , etc.
 
+In the example, we wrote the return statement and a + b on a new line. The engine automatically adds ; to return. So return will always return undefined . Note that there is no automatic insert after if/else . */
