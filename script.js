0c5e0aae6687741dc7d533898c9ec4bd4d2ec7e0
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-30 12:12:59 +0530

    November 04, 2021 - Test: 04

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 03 [November 03, 2021]
+// Test: 04 [November 04, 2021]
 
-console.log(Number(2) === Number(2)); // true
-console.log(Boolean(false) === Boolean(false)); // true
-console.log(Symbol('foo') === Symbol('foo')); // false 
+console.log([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((a, b) => a + b)); // 45
+
 
 /* 
 Explanation:
 
-Each Symbol is completely unique . The purpose of the argument passed to Symbol‚ is to give the Symbol a description. The Symbol value does not depend on the passed argument. When we check for equality, we create two brand new Symbols : the first Symbol('foo') and the second Symbol('foo') . The two values ​​are unique and not equal to each other, Symbol('foo') === Symbol('foo') returns false. */
+The reduce() method executes the callback function once for each element present in the array, except for voids, taking four arguments: the initial value (or the value from the previous callback), the value of the current element, the current index, and the array to be iterated over.
+
+In this example, the reduce() function will sum all elements of . */
 
 
