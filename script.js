346b658c51b68c7aa8f7926149076a136e55c222
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-30 12:06:34 +0530

    November 02, 2021 - Test: 02

--- script.js
+++ script.js
@@ -2,17 +2,16 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 26 [November 01, 2021]
+// Test: 02 [November 02, 2021]
 
-console.log(typeof typeof true); // string
+let num = 10;
+console.log(num != 10 ? 'Yes' : 'No'); // No
 
 /* 
 Explanation:
 
-The  typeof operator  returns a  string containing information about the type of the operand. Let's take a look at our example in action.
+In this problem, the ternary operator was used. Its syntax is: let result = condition ? value1 : value2;
 
-1) typeof true will return the string 'boolean'
-
-2) typeof 'boolean' ( typeof typeof true ) will return information of a string type variable, which is 'string' */
+If the condition is true, then value 1 is returned, otherwise value 2. In our condition , 10 != 10 is false , therefore the second value is returned, and this is No*/
 
 
