227177f23b7f91aeb87f9d23c2743ec7c814c5f9
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-30 11:45:18 +0530

    November 01, 2021 - Test: 01

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 24 [October 31, 2021]
+// Test: 26 [November 01, 2021]
 
-const a = 5;
-console.log(+-a); // -5
+console.log(typeof typeof true); // string
 
 /* 
 Explanation:
 
-In this example, we used unary + . Such a plus + does not change the number (Number) in any way. But if the operand is not a number, the unary plus will convert it to a number.
-5 is a number. The unary + does not convert it in any way, so -5 will be printed to the console . */
+The  typeof operator  returns a  string containing information about the type of the operand. Let's take a look at our example in action.
+
+1) typeof true will return the string 'boolean'
+
+2) typeof 'boolean' ( typeof typeof true ) will return information of a string type variable, which is 'string' */
 
 
