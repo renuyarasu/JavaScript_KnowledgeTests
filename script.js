d1f56412f64a86f7b94d7718931fed9259ffc919
Renuka Prasad <renu.yarasu@gmail.com>
2022-06-30 13:09:26 +0530

    November 09, 2021 - Test: 09

--- script.js
+++ script.js
@@ -2,23 +2,13 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 08 [November 08, 2021]
+// Test: 09 [November 09, 2021]
 
-const obj = { number: 5 };
-
-const func = (x = { ...obj }) => {
-    console.log(x.number *= 3);
-}
-func() // 15
-func() // 15
-func(obj) // 15
-func(obj) // 45
+let obj = { city: 'Hyderabad' };
+console.log(obj.hasOwnProperty('city')); // true
 
 /* 
 Explanation:
-
-In this example, arr is an arrow function that takes x and returns (x, 0, 1, 2). The comma operator is also used here.
-
-The comma operator   executes each of its operands (from left to right) and returns the value of the last operand. Therefore, the last value will be returned , ie 2. */
+The hasOwnProperty method lets us know if an object has a particular property. In our case, the property city. */
 
 
