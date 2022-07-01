c9a81608dd91dec2ad994467d23a5fb658898bdf
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-01 10:50:54 +0530

    November 2021 - Test: 11

--- script.js
+++ script.js
@@ -2,17 +2,14 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 10 [November 10, 2021]
+// Test: 11 [November 11, 2021]
 
-let javascript_tests = 5;
-try {
-    javascript_tests = 5 / 0;
-} catch (error) {
-    javascript_tests = 0 / 5
-}
+let i = 10;
+const addTwo = x => x + 2;
+i = (i++, i = addTwo(i), i *= 3, i += 8, i -= 6);
+console.log(i); // 41
 
-console.log(javascript_tests); // Infinity
-
-/* Explanation: Division by 0 returns Infinity */
+/* Explanation: 
+First we increment the value of i to 11 , then we call the addTwo(2) function and assign the result to the new value, i becomes 13 . After that we multiply the current value of i by 3 , the updated value of i will be 39 . Then we increment the value of i by 8, now the value will be 47 . And finally, we subtract 6 from i and the updated value will be 41. */
 
 
