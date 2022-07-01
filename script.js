98e0640ea7920f678487cafe0d4681ad7e87c077
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-01 17:01:26 +0530

    November 2021 - Test: 16

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 15 [November 15, 2021]
-const arr = [1, 2, 3, 4, 5];
-const result = arr.shift();
-console.log(arr); // [1, 2, 3, 4, 5];
-console.log(result); // 1
+// Test: 16 [November 16, 2021]
+
+console.log(path); // ReferenceError: Cannot access 'path' before initialization
+let path = '/img/logo.png'
 
 /* Explanation: 
-The   shift() method removes   the first   element from the array and returns its value. This method changes the length of an array. */
+console.log does not see the path variable, because it hasn't been declared yet, so it throws an error .
+
+If the path variable were declared using the var keyword, then the answer would be undefined. It has to do with scope. */
 
 
