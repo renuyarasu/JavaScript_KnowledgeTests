a8c51c7e864fcf3467f69f1d5adfd0b0ff7297a3
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-01 16:58:09 +0530

    November 2021 - Test: 14

--- script.js
+++ script.js
@@ -2,16 +2,14 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 13 [November 13, 2021]
-
-console.log(0 || "" || 2 || undefined || true || false); // 2
+// Test: 14 [November 14, 2021]
+const nums = [1, 2, 3, 4, 5];
+const [a] = nums;
+console.log(a); // 1
 
 /* Explanation: 
-Logical operator  ||  performs the following actions:
+In this problem, destructuring is applied . In the variable a we took out the first element of the array (10) and brought it out.
 
-Evaluates operands from left to right.
-Each operand converts to a boolean value. If the result is  true , stops and returns the original value of this operand.
-If all operands are false ( false ), returns the last one.
-In our case, the highest operand will be displayed as true , i.e. 2 */
+In React, destructuring is constantly used in conjunction with the useState hook. For example, const [activeId, setActiveId] = useState() */
 
 
