57fb230debb3a2cdae52195bea49029f64d4a32f
Renuka Prasad <renu.yarasu@gmail.com>
2022-07-01 16:56:01 +0530

    November 2021 - Test: 13

--- script.js
+++ script.js
@@ -2,23 +2,16 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 12 [November 12, 2021]
+// Test: 13 [November 13, 2021]
 
-const compareCars = (car1, car2 = car) => {
-    if (car1 !== car2) {
-        console.log('Car 1 not equal to Car 2');
-    } else {
-        console.log('They are the same');
-    }
-}
-const car = { name: 'BMW' };
-compareCars(car) // They are the same
+console.log(0 || "" || 2 || undefined || true || false); // 2
 
 /* Explanation: 
-Objects are passed by reference. When we test objects for strict equality ( === ), we compare their references. We set the default value for car2 to the car object and pass the car object as the value for car1.
+Logical operator  ||  performs the following actions:
 
-This means that both values ​​have a reference to the same memory location , so they are equal.
-
-The block of code in the else statement is run and printed to the console They are the same */
+Evaluates operands from left to right.
+Each operand converts to a boolean value. If the result is  true , stops and returns the original value of this operand.
+If all operands are false ( false ), returns the last one.
+In our case, the highest operand will be displayed as true , i.e. 2 */
 
 
