f532f37a8f1a4610e14ecf36d8abd11e006b43e7
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-10 22:59:04 +0530

    November 2021 - Test: 29

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 28 [November 28, 2021]
-const result = parseInt('016', 16) - parseInt('04', 8);
-console.log(result); // 22-14 = 18
+// Test: 29 [November 29, 2021]
+
+const maxNumber = Math.pow(10, 1000);
+
+if (maxNumber === Infinity) {
+    console.log('Infinity!');
+}
+console.log(1 / maxNumber);
+
+/* 
+Standard built-in object Infinity 🔥
+
+The global property Infinity is a numeric value representing infinity.
+
+Infinity is a property of the global object, that is, it is a variable in the global scope.
+
+The initial value of Infinity is Number.POSITIVE_INFINITY . The value of Infinity ( positive infinity ) is greater than any other number, including positive infinity itself. This value behaves like a mathematical infinity ; for example, any positive number multiplied by Infinity gives Infinity, and any number divided by Infinity gives 0.
+ */
