c53815ae349274dff4726545c365ff3c4bd793c0
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-10 22:51:05 +0530

    November 2021 - Test: 27

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 26 [November 26, 2021]
-let i = 0;
-for (i; i < 17; i += 4) {
-    if (++i === 7) break
-    else continue
+// Test: 27 [November 27, 2021]
+const myArray = [-3, -2, -1, 0, 1, 2, 3, 4, 5];
+function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0);
+        return false
+    }
+    return num > 1;
 }
-console.log(i++); //20
+
+let result = myArray.filter(isPrime);
+console.log(result); // [2]
