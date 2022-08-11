ccb8fd0487952018322d7ff8df29a5ad79fade68
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-11 21:38:23 +0530

    December 2021 - Test: 01

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 30 [November 30, 2021]
-console.log([1, 2, 3].includes(2)); // true
-console.log([1, 2, 3].includes(4)); // false
-console.log([1, 2, 3].includes(3, 3)); // false
-console.log([1, 2, 3].includes(3, -1)); // true
-console.log([1, 2, 3].includes(NaN)); // false
+// Test: 01 [December 01, 2021]
+
+let myArray = ['JavaScript', 'Knowledge', 'Tests', 1, 2, 3, true];
+let shiftArray = myArray.shift();
+
+console.log(shiftArray); // JavaScript
