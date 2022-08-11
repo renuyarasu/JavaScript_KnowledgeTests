1b13778f0a1ef31ed6daa18e0444ad1a7ccc0952
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-11 21:46:50 +0530

    December 2021 - Test: 04

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 03 [December 02, 2021]
+// Test: 04 [December 02, 2021]
 
-class someClass { };
-const someObj = new someClass();
-
-const result = typeof someObj === typeof someClass;
-console.log(result); // false
+let myArray = [2, 5, 9];
+console.log(myArray.indexOf(2)); // 0
+console.log(myArray.indexOf(7)); // -1
+console.log(myArray.indexOf(9, 2)); // 2
+console.log(myArray.indexOf(2, -1)); // -1
+console.log(myArray.indexOf(2, -3)); // 0
+console.log(myArray.indexOf(2, 5)); // -1
