a361d96fd6a28ca9c04d45eb307cd5f41c5188d0
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-11 21:44:23 +0530

    December 2021 - Test: 03

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 02 [December 01, 2021]
+// Test: 03 [December 02, 2021]
 
-let array_01 = ['01', '02', '03'];
-let array_02 = ['11', '22', '33'];
-let array_03 = array_02.concat(array_01);
-console.log(array_03); // [ '11', '22', '33', '01', '02', '03' ]
+class someClass { };
+const someObj = new someClass();
+
+const result = typeof someObj === typeof someClass;
+console.log(result); // false
