643e8172b2a6f9f68d9e1105a804e3c7ddc959e3
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-11 22:18:13 +0530

    December 2021 - Test: 11

--- script.js
+++ script.js
@@ -2,10 +2,32 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 10 [December 06, 2021]
+// Test: 11 [December 06, 2021]
 
-let a = ['JavaScript | Knowledge Tests'];
-let b = ['JavaScript | Knowledge Tests'];
+let array_01 = [1, 2, [3, 4]];
+console.log(array_01.flat()); // [ 1, 2, 3, 4 ]
 
-console.log(a == b); // false
-console.log(a === b); // false
+
+
+let array_02 = [1, 2, [3, 4, [5, 6]]];
+console.log(array_02.flat()); // [ 1, 2, 3, 4, [ 5, 6 ] ]
+
+
+
+let array_03 = [1, 2, [3, 4, [5, 6]]];
+console.log(array_03.flat(2)); // [ 1, 2, 3, 4, 5, 6 ]
+
+
+
+let array_04 = [1, 2, [3, 4, [5, 6, [7, 8]]]];
+console.log(array_04.flat(Infinity)); // [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+
+
+
+
+/* 
+flat 🔥 method
+
+The flat() method returns a new array in which all elements of the nested subarrays have been recursively "raised" to the specified depth level.
+
+Consider the example in the picture. In the first two cases, the subarrays go up one level, so we reduce the number of nested arrays by one. In the third case, we already raise two levels and get rid of both arrays. If you need to get rid of all subarrays, then inside the brackets you need to pass Infinity (infinity). In this case, all subarrays will be removed. */
