d29a99b51a78a4121ad654d945de599eee5ce34c
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-11 22:28:41 +0530

    December 2021 - Test: 15

--- script.js
+++ script.js
@@ -2,7 +2,22 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 14 [December 08, 2021]
+// Test: 15 [December 08, 2021]
 
-const result = parseInt(1 / 0, 19);
-console.log(result); // 18
+if (01)
+    console.log(01);
+else if (02)
+console.log(02);
+else if (03)
+console.log(03);
+else 
+console.log(10);
+/* 
+if...else 🔥
+
+The if statement executes the statement if the specified condition is true (true). If the condition is not met (false), then another instruction can be executed.
+
+Syntax:
+- A condition is an expression that is either true or false.
+- Instruction1 - instruction to be executed if the "condition" value is true. Can be any statement including nested if. A block ({...}) is used to group multiple statements. When no action is required, an empty statement can be used.
+- Instruction2 - an instruction to be executed if the "condition" value is false. Can be any statement, including nested if. Instructions can also be grouped into a block. */
