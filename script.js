fd61c403e8ed3bcd6ad7d25914d1a4161f67e02b
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-12 13:15:09 +0530

    December 2021 - Test: 26

--- script.js
+++ script.js
@@ -3,11 +3,19 @@
 console.clear();
 // let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
 
-// Test: 25 [December 14, 2021]
+// Test: 26 [December 14, 2021]
 
-// Object.defineProperty method 🔥
+// concat method 
 
-const a = Symbol('JavaScript | Knowledge Tests');
-const b = Symbol('JavaScript | Knowledge Tests');
+let x = ['a', 'b', 'c'];
+let y = ['d', 'e', 'f'];
+let z = x.concat(y);
 
-console.log(a === b); // false
+console.log(z); // [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+/* 
+The concat() method returns a new array consisting of the array it was called on, concatenated with other arrays and/or values ​​passed as arguments.
+
+The concat method creates a new array consisting of the elements in the object it was called on, followed in order, for each argument, by all of its elements (if the argument is an array), or the argument itself (if it is not an array).
+
+The concat method does not modify this array or any of the arrays passed as arguments, but instead returns a shallow copy containing copies of those elements that were concatenated with the original arrays.
+ */
