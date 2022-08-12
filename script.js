375119857374c45e1897bf3e49032406b8ea50f0
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-12 11:09:42 +0530

    December 2021 - Test: 16

--- script.js
+++ script.js
@@ -2,22 +2,14 @@
 // https://t.me/javascript_tests
 console.clear();
 
-// Test: 15 [December 08, 2021]
+// Test: 16 [December 09, 2021]
 
-if (01)
-    console.log(01);
-else if (02)
-console.log(02);
-else if (03)
-console.log(03);
-else 
-console.log(10);
-/* 
-if...else 🔥
-
-The if statement executes the statement if the specified condition is true (true). If the condition is not met (false), then another instruction can be executed.
-
-Syntax:
-- A condition is an expression that is either true or false.
-- Instruction1 - instruction to be executed if the "condition" value is true. Can be any statement including nested if. A block ({...}) is used to group multiple statements. When no action is required, an empty statement can be used.
-- Instruction2 - an instruction to be executed if the "condition" value is false. Can be any statement, including nested if. Instructions can also be grouped into a block. */
+const user = {
+    name: 'VedaGna',
+    age: 5
+}
+for (const value in user) {
+    console.log(value);
+}
+/* name
+age */
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-Infinity
+in
