41257b1a14159d250ad4cb1da5f1332ddf9c3557
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-12 12:45:38 +0530

    December 2021 - Test: 23

--- script.js
+++ script.js
@@ -3,24 +3,7 @@
 console.clear();
 // let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
 
-// Test: 22 [December 12, 2021]
-
-function isBigEnough(element, index, array) {
-    return element >= 10;
-}
-console.log([11, 22, 55].every(isBigEnough)); // true
-console.log([1, 5, 9].every(isBigEnough)); // false
-
-
-/* 
-
-every 🔥 method
-
-The every() method checks if all elements of the array satisfy the condition specified in the passed function.
-
-The every() method invokes the passed callback function once for each element present in the array until it finds one for which callback returns false (a value that becomes false when cast to Boolean). If such an element is found, the every() method will immediately return false. Otherwise, if the callback returns true for all array elements, the every() method will return true. The callback function is called only for array indexes that have values ​​assigned; it is not called for indexes that have been dropped or have never been assigned a value.
-
-The callback function is called with three arguments: the element's value, the element's index, and the array to be traversed.
-
-The every() method does not change the array it was called on.
-*/
+// Test: 23 [December 13, 2021]
+const func = () => { };
+const result = func instanceof Object;
+console.log(result); // true
