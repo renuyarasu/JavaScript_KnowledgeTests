c6ba10fa5743167a04d3f254860ae3c39c9ce9bb
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-12 17:18:48 +0530

    December 2021 - Test: 29

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 console.clear();
 // let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
 
-// Test: 28 [December 16, 2021]
-const arr = [1, 5, 10, 15];
-console.log(arr.fill(0, 2, 4)); // [ 1, 5, 0, 0 ]
+// Test: 29 [December 17, 2021]
+
+const myArray = [1, 2, 3];
+console.log(myArray.reverse()); // [ 3, 2, 1 ]
