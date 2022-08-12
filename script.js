cba8001ce2cb81a0fc0b945c930c26776c6b1733
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-12 12:30:34 +0530

    December 2021 - Test: 21

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 // JavaScript | Knowledge Tests
 // https://t.me/javascript_tests
 console.clear();
+// let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
 
-// Test: 20 [December 11, 2021]
+// Test: 21 [December 12, 2021]
 
-let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
-let popped = fruits.pop();
-console.log(fruits); // [ 'Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries' ]
-console.log(popped); // Blueberries
+let x = 5;
+let y = '5';
+console.log(x +++ y); //55
 
-
-/* 
-pop method 🔥
-
-The pop() method removes the last element from the array and returns its value.
-
-The pop method is not type bound; this method can be called or applied to array-like objects. Objects that do not contain a length property that reflects the last element in a series of consecutive zero-based numeric properties may behave incorrectly.
-
-If you call pop() on an empty array, it will return undefined.
-*/
