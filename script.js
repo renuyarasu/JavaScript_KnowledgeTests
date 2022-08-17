40b62deb62c5af3efc0419bdfc16649163a1a514
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-17 22:14:50 +0530

    December 2021 - Test: 45

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
 // JavaScript | Knowledge Tests
 // https://t.me/javascript_tests
 console.clear();
-// let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
-// let result =
-
-// Test: 44 [December 25, 2021]
-
-let check = function (num) {
-    if (num < MIN || num > MAX) {
-        throw new RangeError('VedaGna ' + MIN + 'Yarasu ' + MAX);
-    }
-};
-try {
-    check(500);
-}
-catch (e) {
-    if (e instanceof RangeError) {
-
-    }
-}
-// RangeError 🔥
 /* 
-A RangeError object represents an error that occurs when a value is not in the set or is out of range.
+let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
+let result =
+console.log(result); 
+*/
 
-A RangeError exception is thrown when an attempt is made to pass a number as an argument to a function that is not within the valid range of the function's argument. It can occur when creating an array with the wrong length through the Array constructor, or when passing bad values ​​to the number methods Number.toExponential(), Number.toFixed(), or Number.toPrecision().
-
-The global RangeError object does not contain any methods of its own, however, it does inherit some methods from the prototype chain.
-
-An example of work is shown in the picture. */
+// Test: 45 [December 25, 2021]
+let myArray = (ele) => (ele, 5, 2, 9);
+let result = myArray(43)
+console.log(result); // 9
