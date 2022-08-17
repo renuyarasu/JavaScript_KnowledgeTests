9aef94c079e037d7363f98ed55a5381b3ad8e93b
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-17 19:39:31 +0530

    December 2021 - Test: 33

--- script.js
+++ script.js
@@ -3,21 +3,23 @@
 console.clear();
 // let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
 
-// Test: 32 [December 18, 2021]
+// Test: 33 [December 19, 2021]
 
-const myObject = {
-    name: 'VedaGna',
-    age: 4,
-    year: 2015
-};
-const copy = Object.assign({}, myObject)
-console.log(copy); // { name: 'VedaGna', age: 4, year: 2015 }
+for (let i = 0; i < 5; i++) {
+    setTimeout(() => console.log(i), 1);
+}
 
+/* 
+0
+1
+2
+3
+4
+ */
 
-// Object.assign() method 🔥
+for (var i = 0; i < 5; i++) {
+    setTimeout(() => console.log(i), 1);
+}
 
-/* The Object.assign() method is used to copy the values ​​of all native enumerable properties from one or more source objects to the target object. After copying, it returns the target object.
-
-The Object.assign() method copies only enumerable and native properties from source objects to the target object. It uses the [[Get]] internal method on the source objects and the [[Set]] internal method on the target object, so it also calls getters and setters. That's why it assigns properties instead of just copying or defining new properties. This behavior can make a method unsuitable for injecting new properties into a prototype if the source objects being infused contain getters. Instead, you should use the Object.getOwnPropertyDescriptor() and Object.defineProperty() methods to copy property definitions to prototypes, including their enumerability.
-
-An example of how the method works is shown in the picture. */
+/* 
+ */
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,3 @@
-9
+for (let i = 0; i < 5; i++) {
+    setTimeout(() => console.log(i), 1);
+}
