93fd15f21dbc77ea1f26a9ce3503457079441284
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-17 22:05:14 +0530

    December 2021 - Test: 43 - getter

--- script.js
+++ script.js
@@ -4,9 +4,25 @@ console.clear();
 // let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
 // let result =
 
-// Test: 42 [December 23, 2021]
+// Test: 43 [December 24, 2021]
 
-let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
+const myObject = {
+    log: ['A', 'B', 'C', 'D'],
+    get latest() {
+        if (this.log.length === 0) {
+            return undefined;
+        }
+        return this.log[this.log.length - 1]
+    }
+}
+console.log(myObject.latest); // D
 
-let result = fruits.sort();
-console.log(result); // ['Apple', 'Avocado', 'Apricots', Banana', 'Blackberries', 'Blueberries'];
+// getter 🔥
+/* 
+The get syntax associates a property of an object with a function that will be called when that property is accessed.
+
+Sometimes it is desirable to allow access to a property that returns a dynamically calculated value, or you may want to reflect the state of an internal variable without the need for explicit method calls. In JavaScript, this can be done using a getter.
+
+It is not possible to have a getter bound to a property and have that property actually contain a value at the same time, although it is possible to use a getter and setter in combination to create a pseudo-property type.
+
+An example is shown in the picture. */
