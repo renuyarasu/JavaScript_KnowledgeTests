725eb761e8874e9f571fae5aaf1a8b09d7dc4218
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-17 22:00:08 +0530

    December 2021 - Test: 41

--- script.js
+++ script.js
@@ -4,29 +4,11 @@ console.clear();
 // let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
 // let result =
 
-// Test: 39 [December 22, 2021]
+// Test: 41 [December 23, 2021]
 
-switch (expression) {
-    case one:
-        break;
-    case two:
-        break;
-    case three:
-        break;
-    case four:
-        break;
-    default:
-        break;
+let myArray = [5, 10, 20];
 
+for (let value of myArray) {
+    value += 2;
+    console.log(value); // 7,12,22
 }
-
-
-// switch construction 🔥
-/* 
-The switch statement compares an expression against the cases listed within it and then executes the corresponding statements.
-
-If the expression matches a case, then the instructions for that case are executed. If multiple cases match a value, only the first case will be used.
-
-First, the program tries to find a suitable case whose value is equal to the value of the search expression (using strict comparison, ===) and then executes the instructions corresponding to the case. If there is no suitable case, the default case is searched, which is optional. If there is no default case, execution continues at the statement immediately following the switch. By convention, the default case is described last, but this is not a strict rule.
-
-The optional break statement exits the switch block. It can be located in each of the cases, but is not required. */
