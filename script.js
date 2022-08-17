cfe0242ef0597abb31b5ddbc2c0255206b78546d
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-17 19:41:56 +0530

    December 2021 - Test: 34

--- script.js
+++ script.js
@@ -3,23 +3,9 @@
 console.clear();
 // let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
 
-// Test: 33 [December 19, 2021]
+// Test: 34 [December 19, 2021]
 
-for (let i = 0; i < 5; i++) {
-    setTimeout(() => console.log(i), 1);
-}
-
-/* 
-0
-1
-2
-3
-4
- */
-
-for (var i = 0; i < 5; i++) {
-    setTimeout(() => console.log(i), 1);
-}
-
-/* 
- */
+let fruits = ['Apple', 'Avocado', 'Apricots'];
+let total = fruits.push('Banana', 'Blackberries', 'Blueberries');
+console.log(fruits); // ['Apple', 'Avocado', 'Apricots','Banana', 'Blackberries', 'Blueberries'];
+console.log(total); // 6
