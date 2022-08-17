b66b04851c95678ca71260367a6d351afadf35dd
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-17 21:57:53 +0530

    December 2021 - Test: 40 - switch construction

--- script.js
+++ script.js
@@ -6,11 +6,27 @@ console.clear();
 
 // Test: 39 [December 22, 2021]
 
-let result = 0;
+switch (expression) {
+    case one:
+        break;
+    case two:
+        break;
+    case three:
+        break;
+    case four:
+        break;
+    default:
+        break;
 
-do {
-    result += 7 -= 4;
-} while (result <= 15);
-console.log(result);
+}
 
-// SyntaxError: Invalid left-hand side in assignment
+
+// switch construction 🔥
+/* 
+The switch statement compares an expression against the cases listed within it and then executes the corresponding statements.
+
+If the expression matches a case, then the instructions for that case are executed. If multiple cases match a value, only the first case will be used.
+
+First, the program tries to find a suitable case whose value is equal to the value of the search expression (using strict comparison, ===) and then executes the instructions corresponding to the case. If there is no suitable case, the default case is searched, which is optional. If there is no default case, execution continues at the statement immediately following the switch. By convention, the default case is described last, but this is not a strict rule.
+
+The optional break statement exits the switch block. It can be located in each of the cases, but is not required. */
