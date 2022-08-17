5fc150cd13438f40ba567d0ce7ff229ee81d5bf0
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-17 21:53:39 +0530

    December 2021 - Test: 39

--- script.js
+++ script.js
@@ -4,23 +4,13 @@ console.clear();
 // let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
 // let result =
 
-// Test: 38 [December 21, 2021]
+// Test: 39 [December 22, 2021]
 
-var obj = {
-    prop: function () { },
-    foo: 'bar',
-};
+let result = 0;
 
-var o = Object.seal(obj);
-obj.foo = 'VedaGna';
+do {
+    result += 7 -= 4;
+} while (result <= 15);
+console.log(result);
 
-Object.defineProperties(obj, 'foo', { get: function () { return 'g'; } });
-
-obj.quaxxor = 'Yarasu';
-delete obj.foo;
-
-// Object.seal() method🔥
-
-/* The Object.seal() method seals an object, preventing new properties from being added to the object and making all existing properties non-configurable. The values ​​of the exposed properties can still change as long as they remain writable.
-
-By default, objects are extensible (new properties can be added to them). Sealing an object prevents new properties from being added to it and makes any existing properties non-configurable. It makes all properties of an object fixed and immutable. Marking all object properties as non-configurable also prevents them from being converted from data properties to access properties and vice versa, but does not prevent the value of data properties from changing. Attempts to remove or add properties to a sealed object, or convert data properties to access properties and vice versa, will fail, either silently or throwing a TypeError exception (usually, but not necessarily, this happens in strict mode). */
+// SyntaxError: Invalid left-hand side in assignment
