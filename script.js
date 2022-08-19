f6ffa9b8634fd7e07e7fd90df6df86b55115008f
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-19 07:18:34 +0530

    December 2021 - encodeURI()

--- script.js
+++ script.js
@@ -7,7 +7,24 @@ let result =
 console.log(result); 
 */
 
-// Test: 46 [December 26, 2021]
+// Test: 47 [December 26, 2021]
 
-const circumference = r => parseFloat(r) * 2.0 * Math.PI;
-console.log(circumference(3));
+const uri = 'https://www.goodreads.com/';
+const encoded = encodeURI(uri);
+
+try {
+    console.log(encoded);
+} catch (e) {
+    console.error(e);
+}
+
+
+// encodeURI() 🔥
+/* 
+The encodeURI() method encodes a Uniform Resource Identifier (URI) by replacing some characters with one, two, three, or four escape sequences representing the UTF-8 encoding of the character (the four escape sequences will only be used for characters consisting of two "surrogate" characters) .
+
+The URI is assumed to be a complete URI, so the method does not encode reserved characters that have special meaning in the URI.
+
+encodeURI replaces all characters except the following with the appropriate UTF-8 escape sequences
+
+An example is shown in the picture. */
