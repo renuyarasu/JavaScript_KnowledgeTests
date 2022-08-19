266135bd0f99400b7926bcaf5ab7a5bcac570b7e
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-19 11:48:01 +0530

    January 2022 - Test: 01

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-// JavaScript | Knowledge Tests
-// https://t.me/javascript_tests
 console.clear();
 
-// Test: 01
+// JavaScript | Knowledge Tests
+// https://t.me/javascript_tests
+
+// Test: 01 [January 01, 2022]
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+let fruits = ['Apple', 'Banana', 'Avocado', 'Apricots', 'Blackberries', 'Blueberries'];
+let result =
+console.log(result); 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+const myArray = [true, 'VedaGna', 5, Infinity];
+const result = myArray.unshift(NaN);
+console.log(result); // 5
