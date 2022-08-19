cd2fce5880a2c7259dfb203dc91c52af32161cda
Renuka Prasad <renu.yarasu@gmail.com>
2022-08-19 08:01:22 +0530

    December 2021 - Object.defineProperties()

--- script.js
+++ script.js
@@ -9,19 +9,26 @@ console.log(result);
 
 // Test: [December 26, 2021]
 
-// Date
+// Object.defineProperties()
 
-let today = new Date();
-console.log(today); // 2022-08-19T02:24:33.513Z
-let birthday = new Date('Friday, 19 August 2022 (IST)'); // 2022-08-18T18:30:00.000Z
-console.log(birthday);
+Object.defineProperties(obj, {
+    'property_01': {
+        value: true,
+        writable: true,
+    },
+    'property_02': {
+        value: 'Hello',
+        writable: false,
+    }
+})
 
-/* Creates an instance of a Date object representing a point in time. The Date object contains the number of milliseconds since January 1, 1970 UTC
+/* The Object.defineProperties() method defines new or modifies existing properties directly on an object by returning that object.
 
-If no arguments were passed, the constructor creates a Date object for the current date and time, according to the system settings.
-
-If at least two arguments are passed, the missing arguments are set to the starting values ​​of day of month 1 and time of midnight.
-
-The Date object provides generic behavior across all platforms. A time value can be passed between systems to represent the same point in time and, if used to create a local date object, will reflect the local time equivalent.
-
-An example of work is shown in the picture.*/
+Options:
+obj - an object on which new properties are defined or existing properties are changed.
+props is an object whose own enumerable properties are descriptors for properties that are created or changed. Property descriptors have the following additional keys:
+configurable is true only if the type of this property descriptor can be changed and if the property can be removed from its containing object.
+The default value is set to false.
+The default value is set to false.
+value - the value associated with the property. Can be any valid JavaScript value (number, object, function, etc.).
+The default value is set to undefined.*/
